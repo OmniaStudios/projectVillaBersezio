57f8b51cbb094148522f5e250ffad398def00be2
SabadellMattia <34937314+ElSabadell@users.noreply.github.com>
2020-06-16 17:13:10 +0200

    Recreated index

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -84,37 +84,28 @@ $(document).ready(function () {
     });
 });
 
-//Scroll
-$(document).ready(function () {
+$(document).ready(function() {
     $('#pagepiling').pagepiling({
-        menu: null,
-        direction: 'vertical',
-        verticalCentered: true,
-        sectionsColor: [],
-        anchors: [],
-        scrollingSpeed: 700,
-        easing: 'swing',
-        loopBottom: false,
-        loopTop: false,
-        css3: true,
-        navigation: {
-            'textColor': '#000',
-            'bulletsColor': '#000',
-            'position': 'right',
-            'tooltips': ['section1', 'section2', 'section3', 'section4']
-        },
-        normalScrollElements: null,
-        normalScrollElementTouchThreshold: 5,
-        touchSensitivity: 5,
-        keyboardScrolling: true,
-        sectionSelector: '.section',
-        animateAnchor: false,
-
-        //events
-        onLeave: function (index, nextIndex, direction) { },
-        afterLoad: function (anchorLink, index) { },
-        afterRender: function () { },
+      menu: '#menu',
+        scrollingSpeed: 300,
+      anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6'],
+      sectionsColor: ['green', 'blue', '#2C3E50', '#39C', 'black', 'green'],
+      navigation: {
+        'position': 'right',
+        'tooltips': ['Page 1', 'Page 2', 'Page 3', 'Pgae 4', 'Page 5', 'Page 6']
+      },
+      afterRender: function() {
+        $('#pp-nav').addClass('custom');
+      },
+      afterLoad: function(anchorLink, index) {
+        if (index > 1) {
+          $('#pp-nav').removeClass('custom');
+        } else {
+          $('#pp-nav').addClass('custom');
+        }
+      }
     });
-});
+  
+  });
 
 
