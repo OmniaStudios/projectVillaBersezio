6a5ec28601097e65eb354d3e43ef9c6e7b3d4604
ElSabadell <mattia.sabadell@gmail.com>
2020-07-01 21:56:59 +0200

    Added Dashboard files

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -145,3 +145,42 @@ $(document).ready(function() {
   $('#exampleModal').on('shown.bs.modal', function () {
     $('#myInput').trigger('focus')
   })
+
+  
+   //=========
+  //Dashboard
+  //=========
+  // Time Script
+function startTime()
+{
+var today=new Date();
+var h=today.getHours();
+var m=today.getMinutes();
+var s=today.getSeconds();
+// add a zero in front of numbers<10
+m=checkTime(m);
+s=checkTime(s);
+document.getElementById('txt').innerHTML=h+":"+m+":"+s;
+t=setTimeout('startTime()',500);
+}
+function checkTime(i)
+{
+if (i<10)
+{
+i="0" + i;
+}
+return i;
+}
+
+
+//ToolTip
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  }) 
+
+//Modal
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  })
+
+ 
