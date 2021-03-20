1f2e841b2ded426149c9df0e26e6cdb80ae952ae
Deblazz <gioco654@gmail.com>
2021-03-21 00:28:00 +0100

    Mamma ho paura, o ho tolto le keys, o ho buttato il progetto. NICE

--- config/keys.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    MongoURI:'mongodb+srv://ParcoVillaBersezio:9QcWPF2twh11bHpX@cluster0-vzz2b.mongodb.net/parcoVillaBersezio?retryWrites=true&w=majority',
-    S3:{
-        AWS_ACCESS_KEY_ID:'AKIA46T4THXYA5GC4AWI',
-        AWS_SECRET_ACCESS_KEY: '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',
-        S3_BUCKET: 'arn:aws:s3:eu-central-1:890393279984:accesspoint/pvb'
-    }
-};
